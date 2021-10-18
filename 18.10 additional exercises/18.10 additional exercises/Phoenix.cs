5f2b5dd4b153899328973e69bae871a76d696490
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-10-19 00:08:29 +0300

    ex.1 done

---
+++ 18.10 additional exercises/18.10 additional exercises/Phoenix.cs
@@ -0,0 +1,21 @@
+﻿namespace _18._10_additional_exercises
+{
+    public class Phoenix
+    {
+        private int _bodyLenght;
+        private double _bodyWidth;
+        private int _wingLenght;
+
+        public Phoenix(int bodyLenght, double bodyWidth, int wingLenght)
+        {
+            _bodyLenght = bodyLenght;
+            _bodyWidth = bodyWidth;
+            _wingLenght = wingLenght;
+        }
+
+        public double totalYears()
+        {
+            return (_bodyLenght * _bodyLenght) * (_bodyWidth + 2 * _wingLenght);
+        }
+    }
+}
---
+++ 18.10 additional exercises/18.10 additional exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _18._10_additional_exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Phoenix count 
+            int n = int.Parse(Console.ReadLine());
+
+            // Making a list to contain all years
+            List<double> totalYearsForPhoenixes = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                // Getting input
+                int bodyLenght = int.Parse(Console.ReadLine());
+                double bodyWidth = double.Parse(Console.ReadLine());
+                int wingLenght = int.Parse(Console.ReadLine());
+
+                // Creating a class phoenix that contains all the info for the bird
+                var phoenix = new Phoenix(bodyLenght, bodyWidth, wingLenght);
+
+                // Adding the year to the list of years
+                totalYearsForPhoenixes.Add(phoenix.totalYears());
+            }
+
+            Console.WriteLine();
+
+            // Iterating through every phoenix and showing the year to the user
+            foreach (var year in totalYearsForPhoenixes)
+            {
+                Console.WriteLine(year);
+            }
+        }
+    }
+}
