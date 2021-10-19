b84538bbf3b5062def96e6b81acbad4c4dc6ce49
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-10-19 17:20:41 +0300

    ex.4 progress

---
+++ 18.10 additional exercises/ex.4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ex._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> squads = new Dictionary<string, List<string>>();
+            Dictionary<string, int> squadmates = new Dictionary<string, int>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Trim().Split(" -> ").ToArray();
+            
+                if (input[0] == "Blaze it!") break;
+            
+                string creature = input[0];
+                string squadmate = input[1];
+
+                if (creature == squadmate)
+                    continue;
+
+                else if (squads.ContainsKey(creature) && squads[creature].Contains(squadmate) &&
+                         squads.ContainsKey(squadmate) && squads[squadmate].Contains(creature) && squadmates[creature] != 0)
+                    squadmates[creature] -= 1;
+
+                else if (squads.ContainsKey(creature) && !squads[creature].Contains(creature))
+                    squadmates[creature]++;
+
+                else if (squads.ContainsKey(creature))
+                {
+                    squads[creature].Add(squadmate);
+                    squadmates[creature]++;
+                }
+
+                else
+                {
+                    squads.Add(creature, new List<string>());
+                    squadmates.Add(creature, 0);
+                }
+            }
+
+            Console.WriteLine();
+            foreach (var squadmate in squadmates.OrderByDescending(n => n.Value))
+            {
+                Console.WriteLine($"{squadmate.Key}: {squadmate.Value}");
+            }
+        }
+    }
+}
