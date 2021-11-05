528aaa95986b4eaad5a42bc924522fd6a2665c9d
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-11-05 19:11:49 +0200

    homework done

---
+++ 05.11 Do_while exercises/ex.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace ex._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            int validNums = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                while (input != 0)
+                {
+                    counter++;
+                    input /= 10;
+                }
+
+                if(counter == k)
+                    validNums++;
+
+                counter = 0;
+            }
+
+            Console.WriteLine(validNums);
+        }
+    }
+}
