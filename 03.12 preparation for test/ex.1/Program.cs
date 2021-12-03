dd95f0e5c7205df1d13a20d93ce85f673a6053df
Kaloyan <kaloyangfx@gmail.com>
2021-12-03 11:40:06 +0200

    ex.1

---
+++ 03.12 preparation for test/ex.1/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    double pricePerLiter = double.Parse(Console.ReadLine());
+    double champagneLiter = double.Parse(Console.ReadLine());
+    double waterLiter = double.Parse(Console.ReadLine());
+    double whiskeyLiter = double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"{Sum(pricePerLiter, champagneLiter, waterLiter, whiskeyLiter):F2}");
+}
+
+double Sum(double pricePerLiter, double champagneLiter, double waterLiter, double whiskeyLiter)
+{
+    double champagnePrice = champagneLiter * pricePerLiter;
+    double waterPrice = waterLiter * (pricePerLiter / 4);
+    double whiskeyPrice = whiskeyLiter * (pricePerLiter * 2.3);
+
+    return champagnePrice + waterPrice + whiskeyPrice;
+}
