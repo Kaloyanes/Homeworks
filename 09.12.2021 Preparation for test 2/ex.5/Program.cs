d7ab294ecb5f92d847e756c66b587f0afdc82e19
Kaloyan <kaloyangfx@gmail.com>
2021-12-09 13:21:55 +0200

    ex.5

---
+++ 09.12.2021 Preparation for test 2/ex.5/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        if (j % 3 != 0)
+            continue;
+
+        for (int k = 0; k < 10; k++)
+        {
+            if (k != 4 || k != 7)
+                continue;
+
+            for (int l = 0; l < 10; l++)
+            {
+                if (i + l != n)
+                    continue;
+
+                Console.WriteLine($"{i}{j}{k}{l}");
+            }
+        }
+    }
+}
