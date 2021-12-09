0cddf892a892562a6bdd0c2d8c82958af9a3abdb
Kaloyan <kaloyangfx@gmail.com>
2021-12-09 12:13:32 +0200

    ex.2

---
+++ 09.12.2021 Preparation for test 2/ex.2/Program.cs
@@ -0,0 +1,21 @@
+﻿string Calculate(double budget, double priceFirst)
+{
+    double priceSecond = priceFirst * 0.2;
+    double priceThird = priceSecond * 0.7;
+    double priceFourth = priceSecond * 0.5;
+
+    double price = priceFirst + priceSecond + priceThird + priceFourth;
+    budget -= price;
+
+    if (budget > 0)
+        return $"Yes! {budget:F2} leva left";
+
+    return $"No! {Math.Abs(budget):F2} leva needed";
+}
+
+{
+    double budget = double.Parse(Console.ReadLine());
+    double priceFirst = double.Parse(Console.ReadLine());
+
+    Console.WriteLine(Calculate(budget, priceFirst));
+}
