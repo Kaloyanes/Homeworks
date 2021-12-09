eaa7717af98ed8b0d6c4e0e38bd8bb3632c116b8
Kaloyan <kaloyangfx@gmail.com>
2021-12-09 11:56:03 +0200

    ex.1

---
+++ 09.12.2021 Preparation for test 2/ex.1/Program.cs
@@ -0,0 +1,23 @@
+﻿double Calculate(double priceLaminate, double Width, double Lenght, double pricePad)
+{
+    double area = Width * Lenght;
+    double laminate = area * priceLaminate;
+    double pad = area * pricePad;
+
+    double price = laminate + pad;
+
+    price = price + (price * 0.4);
+
+    return price;
+}
+
+{
+    double priceLaminate = double.Parse(Console.ReadLine());
+
+    double Width = double.Parse(Console.ReadLine());
+    double Lenght = double.Parse(Console.ReadLine());
+
+    double pricePad = double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"{Calculate(priceLaminate, Width, Lenght, pricePad):F2}");
+}
