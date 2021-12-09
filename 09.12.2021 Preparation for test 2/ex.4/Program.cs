4eb8da327352dead9cc1097b0b585c9c7fbbf90b
Kaloyan <kaloyangfx@gmail.com>
2021-12-09 13:09:37 +0200

    ex.4

--- 09.12.2021 Preparation for test 2/ex.4/Program.cs
+++ 09.12.2021 Preparation for test 2/ex.4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+    int count = int.Parse(Console.ReadLine());
+
+    List<double> scales = new List<double>()
+    {
+        0,0,0,0
+    };
+
+    for (int i = 0; i < count; i++)
+    {
+        double input = double.Parse(Console.ReadLine());
+
+        if (input > 0 && input<= 4)
+            scales[0]++;
+
+        else if (input <= 6)
+            scales[1]++;
+
+        else if (input <= 7)
+            scales[2]++;
+
+        else
+            scales[3]++;
+    }
+
+    for (int i = 0; i < scales.Count; i++)
+        scales[i] = scales[i] / count * 100;
+
+    Console.WriteLine($"Light: {scales[0]:F2}%");
+    Console.WriteLine($"Moderate: {scales[1]:F2}%");
+    Console.WriteLine($"Strong: {scales[2]:F2}%");
+    Console.WriteLine($"Very strong: {scales[3]:F2}%");
+}
