cc8a4a0aaef780e367bfd159c5bf9ec96f9ce3d4
Denoic <kaloyangfx@gmail.com>
2022-03-11 13:19:58 +0200

    ex.1 and 2

---
+++ 11.03.2022 Dictionary/ex.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<string, int> dict = new Dictionary<string, int>();
+string[] words = Console.ReadLine().Trim().Split().ToArray();
+
+foreach (string word in words)
+{
+    var lower = word.ToLower();
+
+    if(!dict.ContainsKey(lower))
+        dict.Add(lower, 0);
+
+    dict[lower]++;
+}
+
+foreach (var pair in dict.Where(x => x.Value % 2 != 0))
+{
+    Console.Write($"{pair.Key}, ");
+}
---
+++ 11.03.2022 Dictionary/ex.2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+// Global variable
+Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+void AddPhone(string name, string phoneNumber)
+{
+    Console.WriteLine($"Added {name} to the phonebook.");
+
+    if (!phonebook.ContainsKey(phoneNumber))
+    {
+        phonebook.Add(name, phoneNumber);
+        return;
+    }
+
+    phonebook[name] = phoneNumber;
+}
+
+void SearchPhone(string contact)
+{
+    if (!phonebook.ContainsKey(contact))
+    {   
+        Console.WriteLine($"Contact {contact} does not exist.");
+        return;
+    }
+
+    Console.WriteLine($"{contact} -> {phonebook[contact]}");
+}
+
+while (true)
+{
+    // Взимаме командите и аргументите
+    // Използваме Array понеже използва по-малко ресурси от List-a
+    string[] command = Console.ReadLine().Trim().Split().ToArray();
+
+    // Добре де признавам, че в случая switch е по-добре
+    switch (command[0].ToLower())
+    {
+        case "a":
+            AddPhone(command[1], command[2]);
+            break;
+
+        case "s":
+            SearchPhone(command[1]);
+            break;
+
+        case "end":
+            return;
+
+        default:
+            Console.WriteLine("Unknown Command");
+            break;
+    }
+    
+    Console.WriteLine("");
+}
