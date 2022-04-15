a740f9af5d404784d11b05354ea38d08dd5b4bd5
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 15:57:10 +0300

    ex.3

---
+++ 15.04.2022 String/ex.3/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine().Trim();
+string inputUp = input.ToUpper();
+
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == inputUp[i])
+        count++;
+}
+
+Console.WriteLine(count);
