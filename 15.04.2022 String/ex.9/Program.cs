277ebf3c680e0d1a6332ccd3c46c52bba0ebf578
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 16:12:31 +0300

    ex.9

---
+++ 15.04.2022 String/ex.9/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+int num;
+
+// Check-ваме ако можем да parse-нем тогава няма букви, а ако не можем да parse-нем тогава има букви
+// По-бързо и лесно отколкото да имаме 2 цикъла според мен
+bool CanParse = int.TryParse(input.ToString(), out num);
+
+if (!CanParse)
+{
+    Console.WriteLine("Wrong input");
+    return;
+}
+
+foreach (var c in input)
+{
+    int n = int.Parse(c.ToString());
+
+    Console.WriteLine(new string('*', n));
+}
