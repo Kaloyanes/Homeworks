2d6905be83a1a25f1be0c91e351c21beb9ca4f3d
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 16:12:28 +0300

    ex.8

---
+++ 15.04.2022 String/ex.8/Program.cs
@@ -0,0 +1,13 @@
+﻿string input = Console.ReadLine().Trim();
+
+char[] inputArr = input.ToCharArray();
+Array.Reverse(inputArr);
+string reversed = new string(inputArr);
+
+if (input == reversed)
+{
+    Console.WriteLine("yes");
+    return;
+}
+
+Console.WriteLine("no");
