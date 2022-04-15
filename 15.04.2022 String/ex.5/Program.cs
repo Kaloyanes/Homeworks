96871b3ecd7dfe086cceacb00afd53aa7d862b35
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 15:57:17 +0300

    ex.5

---
+++ 15.04.2022 String/ex.5/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+int count = 0;
+
+foreach (var c in input)
+{
+    int num;
+
+    if (int.TryParse(c.ToString(), out num))
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
