0064d3d0231b4fef002ee785c8083d821b6a8003
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 15:57:13 +0300

    ex.4

---
+++ 15.04.2022 String/ex.4/Program.cs
@@ -0,0 +1,12 @@
+﻿char[] chars = new[] { 'a', 'e', 'o', 'u', 'i', 'y' };
+
+string input = Console.ReadLine();
+int count = 0;
+
+foreach (char c in input)
+{
+    if(chars.Contains(c))
+        count++;
+}
+
+Console.WriteLine(count);
