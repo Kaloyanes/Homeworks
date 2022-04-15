79263002a8b9a2b012edfa2ee05d88e36f379c05
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-04-15 16:12:35 +0300

    ex.10

---
+++ 15.04.2022 String/ex.10/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<char, int> dict = new Dictionary<char, int>();
+
+string input = Console.ReadLine();
+foreach (char c in input)
+{
+    if (!dict.ContainsKey(c))
+    {
+        dict.Add(c, 1);
+    }
+}
+
+Console.WriteLine(dict.Sum(i => i.Value));
